9e38ebd878781dcf55d4fb7aef4cd062bbb8c50f
Beresneva Katya <beresnevakatya@MacBook-Air-Beresneva.local>
2021-02-11 12:50:14 +0300

    start

---
+++ src/components/CreateContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="formHandler">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="nameForm"/>
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="text" v-model="phoneForm"/>
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="text" v-model="emailForm"/>
+    </div>
+    <input type="submit" @click="addContact(nameForm, phoneForm, emailForm)">
+     
+  </form>
+</template>
+
+<script>
+export default {
+  name: "new-contact", // необезательное свойство 
+  inject: ["addContact"],
+  data() {
+    return {
+      nameForm: '',
+      phoneForm: '',
+      emailForm: '',
+    };
+  },
+
+  methods: {
+    // addContact(event){
+    //   event.preventDefault();
+
+    //   // this.$emit("add-contact", this.nameForm, this.phoneForm, this.emailForm)
+    // }
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,53 @@
+<template>
+  <li>
+    <h2>
+      <span>{{ friends[id].name }} ({{ friends[id].valid ? "valid" : "invalid" }})</span>
+      <button class="delete" @click="deleteContact(id)">X</button>
+    </h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleValid(id)">
+      Toggle Valid
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ friends[id].phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friends[id].email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  inject: ["friends", "toggleValid", "deleteContact"],
+  name: "contact", // необезательное свойство 
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.delete {
+  /* если что так делать нельзя */
+  color: red !important;
+  background:transparent !important;
+  border: none !important;
+  box-shadow: none !important;
+  float: right;
+}
+</style>
